a4db98c8bcea794225a0a1ae8ded6a99cc32ed89
三咲智子 Kevin Deng <sxzz@sxzz.moe>
2023-11-28 19:35:28 +0800

    chore: update

--- vue.config.js
+++ vue.config.js
@@ -1,6 +1,4 @@
 const { defineConfig } = require('@vue/cli-service')
-
-/** @type {typeof import("unplugin-vue-macros/webpack").default} */
 const VueMacros = require('unplugin-vue-macros/webpack')
 
 module.exports = defineConfig({
